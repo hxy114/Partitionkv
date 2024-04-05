af1cdbafe20ba43afaa3545aac0cf535797893cc
hahah <1569635683@qq.com>
2024-04-05 16:03:02 +0800

    none

---
+++ db/pmtable.h
@@ -0,0 +1,20 @@
+#ifndef PMTABLE
+#define PMTABLE
+#include"skiplist.h"
+
+
+class PMLog{
+
+};
+
+class PMtable{
+private:
+    PMLog *PMLog;
+    leveldb::SkipList<uint64_t,char*> *skiplist;
+
+
+
+};
+
+
+#endif
